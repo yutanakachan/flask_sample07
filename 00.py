365174aa47c4171ce64c0943ad1b627d6c8da166
Yuta <y.nakagawa1107@gmail.com>
2018-12-01 15:20:42 +0900

    login機能　搭載

--- 00.py
+++ 00.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,20 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World"
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return '''
+        <form action="/login" method="post">
+            Password: <input type="text"><br>
+            <input type="submit">
+        </form>
+        '''
+
+    if request.method == "POST":
+        return "Logged in"
 
 
 if __name__ == "__main__":
