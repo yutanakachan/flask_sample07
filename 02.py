b5fb020b7944a6195180fcb6fc95e81930684ccd
mimumeg <meg.stella555@gmail.com>
2018-12-01 17:10:10 +0900

    メンバーリストを追加した。

--- 02.py
+++ 02.py
@@ -21,5 +21,11 @@ def omikuji():
     return render_template("omikuji.html", result=result)
 
 
+@app.route("/members")
+def members():
+    members = ["Bob", "Tom", "Ken"]
+
+    return render_template("members.html", members=members)
+
 if __name__ == "__main__":
     app.run(debug=True)
