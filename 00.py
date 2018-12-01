0b51e67fb473a0b8e0292bfbaecd131fa7c04d20
Yuta <y.nakagawa1107@gmail.com>
2018-12-01 16:01:46 +0900

    名前をHTMLでも実行出来るように実践

--- 00.py
+++ 00.py
@@ -24,3 +24,4 @@ def login():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
--- 02.py
+++ 02.py
@@ -8,5 +8,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/user/<name>")
+def hello(name):
+    return render_template("hello.html", name=name)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
